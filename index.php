aa664e6a0e90433093c6ef4a7e2946ad68423763
ZachyDev <mosetizachary001@gmail.com>
2020-03-13 20:26:58 +0300

    arithmetic operators

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+// definining a constant-the value cannot be changed later in the code
+    define("PHP_DOC","PHP Documentation  <a href='phpinfo()'>Documentation</a>");
+    echo PHP_DOC;
+?>
